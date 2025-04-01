ae6c943d557b3d27579b64be875dbe99b4a5834b
peterland7572 <113414292+peterland7572@users.noreply.github.com>
2025-04-01 19:35:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def dooray_webhook():
 
         # 🚀 Dooray가 인식할 수 있는 응답 포맷
         response_data = {
-            "text": "(dooray://3570973280734982045/members/3790034441950345057 \"member\")조현웅/SGE 품질검증팀",
+            "text": "(dooray://3570973280734982045/members/3790034441950345057 \"member\")[조현웅/SGE 품질검증팀]  Here",
             "responseType": "inChannel"  # ephemeral = 사용자에게만 보이는 응답
         }
 
