1b3a2e4d51230d3e648eba99eb0bde3aa55a7126
peterland7572 <113414292+peterland7572@users.noreply.github.com>
2025-04-02 16:16:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -38,12 +38,12 @@ def dooray_webhook():
             }
         }
 
-        headers = {"token": cmd_token}
-        response = requests.post(responseUrl, json=dialog_data, headers=headers)
+        #headers = {"token": cmd_token}
+        #response = requests.post(responseUrl, json=dialog_data, headers=headers)
 
         if response.status_code == 200:
             logger.info("✅ Dialog 생성 요청 성공")
-            return jsonify({"responseType": "ephemeral", "text": "📢 업무 입력 창을 열었습니다."}), 200
+            return jsonify({"responseType": "ephemeral", "text": dialog_data }), 200
         else:
             logger.error("❌ Dialog 생성 요청 실패: %s", response.text)
             return jsonify({"responseType": "ephemeral", "text": "⚠️ 업무 입력 창을 여는 데 실패했습니다."}), 500
